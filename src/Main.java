1c7284974c897b57b1d28615de51810296483331
wyfun <wyfun@outlook.pt>
2025-01-28 21:50:00 +0000

    Solution

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-        int[] result =twoSum(nums, target);
+        twoSum(nums, target);
         System.out.println("Indices: [" + nums[0] + ", " + nums[1] + "]"+"="+(nums[0]+nums[1]));
     }
 
